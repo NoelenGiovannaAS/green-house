8c03714b60ed5851a7844afa28e84bb82e58fcf4
Noh <noelen_g@outlook.com>
2023-09-02 18:16:42 -0300

    criação da estrutura base do projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       Teste
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/router/router.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <h1>Teste</h1>,
+      children: [],
+    },
+  ]);
